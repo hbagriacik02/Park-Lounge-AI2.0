2d843058567c0015d4fdbd5250982713cec84a98
paal <paal@example.com>
2025-04-09 08:45:21 +0200

    added button, led, infrared sensor to setup

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
 #include "Servo.h"
 #include "lcd1602.h"
+#include "ir_sensor.h"
+#include "button.h"
+#include "led.h"
 #include "state_machine.h"
+
+// Verwendete Pins
+//  Servo: Pin 13
+//  LCD: SDA = Pin 21, SCL = Pin 22
+//  IR-Sensor: Pin 27
+//  Rote LED: Pin 14
+//  Grüne LED: Pin 25
+//  Button: Pin 26
 
 lcd1602 lcd;            // Das Display-Objekt
 ServoControl servo(13); // Servo an Pin 13
-
-// put function declarations here:
-// int myFunction(int, int);
+IRSensor irSensor(27);  // IR-Sensor an Pin 27
+LED redLED(14);         // Rote LED an Pin 14
+LED greenLED(25);       // Grüne LED an Pin 25
+Button button(26);      // Button an Pin 26
 
 void setup()
 {
@@ -18,14 +30,14 @@ void setup()
   Serial.println("LCD initialisiert");
   servo.begin(); // Initialisiert den Servo
   Serial.println("Servo initialisiert");
+  irSensor.begin(); // Initialisiert den IR-Sensor
+  Serial.println("IR-Sensor initialisiert");
+  redLED.begin();
+  greenLED.begin();
+  button.begin();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
 }
-
-// put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
